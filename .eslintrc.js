176455d5ff8c626b2c7707d08845f7cfcf2976ea
Jose Luis Represa <joseluis.represa@bbva.com>
2019-12-04 13:04:35 +0100

    chore(files): update common files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,51 @@
-module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
-  env: {
-    browser: true
-  },
-  rules: {
-    'ember/no-jquery': 'error'
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
+'use strict';
 
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      })
-    }
-  ]
+module.exports = {
+	root: true,
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module'
+	},
+	plugins: [
+		'ember'
+	],
+	extends: [
+		'plugin:ember/recommended',
+		'eslint-config-bbva'
+	],
+	env: {
+		browser: true
+	},
+	overrides: [{
+		files: [
+			'.huskyrc.js',
+			'.commitlintrc.js',
+			'.eslintrc.js',
+			'.template-lintrc.js',
+			'ember-cli-build.js',
+			'index.js',
+			'testem.js',
+			'blueprints/*/index.js',
+			'config/**/*.js',
+			'tests/dummy/config/**/*.js'
+		],
+		excludedFiles: [
+			'addon/**',
+			'addon-test-support/**',
+			'app/**',
+			'tests/dummy/app/**'
+		],
+		parserOptions: {
+			sourceType: 'script',
+			ecmaVersion: 2015
+		},
+		env: {
+			browser: false,
+			node: true
+		},
+		plugins: ['node'],
+		rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+			'no-process-env': 0
+		})
+	}]
 };
